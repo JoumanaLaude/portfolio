50285dcb5834a789dd83899ad0f1dbdf1c07b531
Joumana Laude <30224508+JoumanaLaude@users.noreply.github.com>
2022-01-21 14:20:42 -0500

    project page updates

--- src/components/Projects/Cards.js
+++ src/components/Projects/Cards.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
 import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faReact, faSass, faHtml5, faCss3Alt, faJs, faBootstrap, faWordpress, faMdb, faFigma, faNode } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faReact, faSass, faHtml5, faCss3Alt, faJsSquare, faBootstrap, faWordpress, faMdb, faFigma, faGit, faYarn, faNode } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Animated from './Animated';
 
@@ -27,45 +27,49 @@ const ProjectComponent = ({ data }) => {
         <Grid>
             <section>
                 <animated.div style={fade}>
-                    <h1>Projects & Skills</h1>
-                    <p>Here are some of my personal projects. As I learn more, I update and improve what I have while also working on new projects.</p>
+                    <h1>Projects & Skillset</h1>
+                    <p>Here are some of my personal projects. As I learn more, I update and improve what I have while also working on new projects. My main focus is currently front-end development, but I would like to later transition into full stack.</p>
                 </animated.div>
                 <animated.div style={tech}>
-                <center><p>Technologies I have worked with:</p></center>
-                <Skills>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faReact} className="fa-3x fa-fw" alt="React" /></Animated><p className="icon-name">React</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faSass} className="fa-3x fa-fw" alt="Sass" /></Animated><p className="icon-name">Sass</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faHtml5} className="fa-3x fa-fw" alt="HTML5" /></Animated><p className="icon-name">HTML5</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faCss3Alt} className="fa-3x fa-fw" alt="CSS3" /></Animated><p className="icon-name">CSS3</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faJs} className="fa-3x fa-fw" alt="JavaScript" /></Animated><p className="icon-name">JavaScript</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faBootstrap} className="fa-3x fa-fw" alt="Bootstrap" /></Animated><p className="icon-name">Bootstrap</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faWordpress} className="fa-3x fa-fw" alt="WordPress" /></Animated><p className="icon-name">WordPress</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faFigma} className="fa-3x fa-fw" alt="Figma" /></Animated><p className="icon-name">Figma</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faMdb} className="fa-3x fa-fw" alt="MongoDB" /></Animated><p className="icon-name">MongoDB</p></div>
-                    <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faNode} className="fa-3x fa-fw" alt="NodeJS" /></Animated><p className="icon-name">NodeJS</p></div>
-                </Skills>
-            </animated.div>
-            <animated.div style={projects}>
-                {data.map((projects, index) => {
-                    return (
-                        <div key={index} className="card">
-                            <a href={projects.link} target="_blank" rel="noreferrer noopener" aria-label="Launch site">
-                                <Img src={projects.img} alt={projects.title} /></a>
-                            <div className="card-content">
-                                <Title>{projects.title}</Title>
-                                <p>{projects.tools}<br /><br />{projects.desc}</p>
-                                <div className="icon-link">
-                                    <a href={projects.link} target="_blank" rel="noreferrer noopener" aria-label="Launch site" className="icon-link">
-                                        <FontAwesomeIcon icon={faExternalLinkAlt} className="fa-2x fa-fw" /></a>
-                                    <a href={projects.github} target="_blank" rel="noreferrer noopener" aria-label="Github" className="icon-link">
-                                        <FontAwesomeIcon icon={faGithub} className="fa-2x fa-fw" /></a>
+                    <center><p>Technologies I have worked with:</p></center>
+                    <Skills>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faReact} className="fa-3x fa-fw" alt="React" /></Animated><IconName>React</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faSass} className="fa-3x fa-fw" alt="Sass" /></Animated><IconName>Sass</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faHtml5} className="fa-3x fa-fw" alt="HTML5" /></Animated><IconName>HTML5</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faCss3Alt} className="fa-3x fa-fw" alt="CSS3" /></Animated><IconName>CSS3</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faJsSquare} className="fa-3x fa-fw" alt="JavaScript" /></Animated><IconName>JavaScript</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faBootstrap} className="fa-3x fa-fw" alt="Bootstrap" /></Animated><IconName>Bootstrap</IconName></div>
+                    </Skills>
+                    <Skills>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faWordpress} className="fa-3x fa-fw" alt="WordPress" /></Animated><IconName>WordPress</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faFigma} className="fa-3x fa-fw" alt="Figma" /></Animated><IconName>Figma</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faGit} className="fa-3x fa-fw" alt="Git" /></Animated><IconName>Git</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faYarn} className="fa-3x fa-fw" alt="Yarn" /></Animated><IconName>Yarn</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faMdb} className="fa-3x fa-fw" alt="MongoDB" /></Animated><IconName>MongoDB</IconName></div>
+                        <div><Animated rotation={20} timing={200}><FontAwesomeIcon icon={faNode} className="fa-3x fa-fw" alt="NodeJS" /></Animated><IconName>NodeJS</IconName></div>
+                    </Skills>
+                </animated.div>
+                <animated.div style={projects}>
+                    {data.map((projects, index) => {
+                        return (
+                            <div key={index} className="card">
+                                <a href={projects.link} target="_blank" rel="noreferrer noopener" aria-label="Launch site">
+                                    <Img src={projects.img} alt={projects.title} /></a>
+                                <div className="card-content">
+                                    <Title>{projects.title}</Title>
+                                    <p>{projects.tools}<br /><br />{projects.desc}</p>
+                                    <div className="icon-link">
+                                        <a href={projects.link} target="_blank" rel="noreferrer noopener" aria-label="Launch site" className="icon-link">
+                                            <FontAwesomeIcon icon={faExternalLinkAlt} className="fa-2x fa-fw" /></a>
+                                        <a href={projects.github} target="_blank" rel="noreferrer noopener" aria-label="Github" className="icon-link">
+                                            <FontAwesomeIcon icon={faGithub} className="fa-2x fa-fw" /></a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
-            </animated.div>
-        </section>
+                        )
+                    })}
+                </animated.div>
+            </section>
         </Grid >
     )
 }
@@ -83,11 +87,18 @@ const Grid = styled.div`
 
 const Skills = styled.div`
     display: flex;
-    flex-direction: row;
-    display: flex;
-    justify-content: space-between;
+    justify-content: space-around;
     flex-wrap: wrap;
-    padding-top: 1rem;
+`;
+
+const IconName = styled.p`
+    display: flex;
+    justify-content: center;
+    align-self: center;
+    padding-top: 0;
+    @media only screen and (max-width: 900px) {
+        font-size: .8rem;
+    }
 `;
 
 const Img = styled.img`
--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,12 +1,13 @@
 import space from "./images/explore.png"
 import cafe from "./images/clichecafe.png"
+import portfolio from "./images/portfolio.png"
 import mixormatch from "./images/mix-or-match.png"
 
 export const projects = [
     {
         title: "Spacetagram",
         img: space,
-        tools: "React, NASA API, CSS Grid",
+        tools: "React, NASA API, CSS Grid, React Router DOM, styled-components",
         desc: "Responsive web app with custom CSS that fetches images and videos from NASA's API that date back to 1995.",
         github: "https://github.com/JoumanaLaude/spacetagram-v2",
         link: "https://prettyspacious.netlify.app/"
@@ -14,10 +15,18 @@ export const projects = [
     {
         title: "Cliche Cafe",
         img: cafe,
-        tools: "React, Reactstrap",
+        tools: "React, Hooks, Reactstrap",
         desc: "First solo React project using hooks and libraries. Created the UI, food menus, and included a shopping cart for online ordering.",
         github: "https://github.com/JoumanaLaude/cliche-cafe-react",
         link: "https://clichecafe.netlify.app/"
+    },
+    {
+        title: "Personal Portfolio",
+        img: portfolio,
+        tools: "React, CSS Grid, Flexbox, React Spring, React Router DOM",
+        desc: `Since this is a *personal* portfolio I created a responsive layout from scratch with custom CSS and media queries. I used a simple design with a theme that fits what I personally like. I have separate page components for the "about" and "projects" sections for easier scrolling.`,
+        github: "https://github.com/JoumanaLaude/portfolio",
+        link: "https://joumana.netlify.app/"
     },
     {
         title: "Halloween Mix-or-Match (Clone)",
